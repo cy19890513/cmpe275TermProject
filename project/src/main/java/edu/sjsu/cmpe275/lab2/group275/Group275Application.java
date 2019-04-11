fee54edd8f09edbd6f8de1f612c83cfcd8fdc7ed
Rui Li <rui@XXX.local>
2019-04-11 15:26:33 -0700

    add framework

---
+++ project/src/main/java/edu/sjsu/cmpe275/lab2/group275/Group275Application.java
@@ -0,0 +1,13 @@
+package edu.sjsu.cmpe275.lab2.group275;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Group275Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Group275Application.class, args);
+    }
+
+}
---
+++ project/src/test/java/edu/sjsu/cmpe275/lab2/group275/Group275ApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.sjsu.cmpe275.lab2.group275;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Group275ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
