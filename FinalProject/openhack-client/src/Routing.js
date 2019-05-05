d038bde5f10d0f34cbcfc9620bb46131fcb2685f
Yang Chen <yang.chen@mypenguinsmart.com>
2019-05-05 12:00:33 -0700

    hackathon list init

--- FinalProject/openhack-client/src/Routing.js
+++ FinalProject/openhack-client/src/Routing.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+import HackathonList from './hackathon/HackathonList';
+
 function Routing() {
   return (
     <Router>
       <Switch>
         <Route exact path={'/'} component={null} />
-          <Route path={'/hackathons'} component=HackathonList />
+          <Route path={'/hackathons'} component={HackathonList} />
           <Route path={'/login'} component={null} />
           <Route path={'/register'} component={null} />
       </Switch>
