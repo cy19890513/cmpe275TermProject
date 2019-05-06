7e8c5d8d4caaa05691486385f405767c0ae5500f
Yang Chen <yang.chen@mypenguinsmart.com>
2019-05-05 23:21:52 -0700

    merge master and yang_temp1

--- FinalProject/openhack-client/src/Routing.js
+++ FinalProject/openhack-client/src/Routing.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from './login/Login';
-<<<<<<< HEAD
-import UserProfile from './userProfile/userProfile'
-||||||| merged common ancestors
-=======
+
+
+
 import Signup from './register/Signup';
 import CreateHackathon from './createHackathon/CreateHackathon';
 
->>>>>>> 3dd82cb75a9112845a55a57cb854e14a63676be3
+
 
 import HackathonList from './hackathon/HackathonList';
 import HackathonEvent from "./hackathon/HackathonEvent";
