0918d0c9fd5d7e14940711e5a9ee49c1083b6ef9
hanhan <hanhan@hanhans-MacBook-Air.local>
2019-05-18 00:58:48 -0700

    update

--- FinalProject/openhack/src/main/java/edu/cmpe275/group275/openhack/service/HackathonServiceImpl.java
+++ FinalProject/openhack/src/main/java/edu/cmpe275/group275/openhack/service/HackathonServiceImpl.java
@@ -167,7 +167,7 @@ public class HackathonServiceImpl implements HackathonService{
         String email = judge.getEmail();
         SimpleMailMessage message = new SimpleMailMessage();
         // String to = email;
-        String to = "jiehanyao0918" +"+"+judge.getUsername()+"@gmail.com";
+        String to = judge.getEmail();
         long uid = judge.getId();
         System.out.println("judge uid: "+uid);
         String text = "Dear " + judge.getUsername() + ", \n\n" +
