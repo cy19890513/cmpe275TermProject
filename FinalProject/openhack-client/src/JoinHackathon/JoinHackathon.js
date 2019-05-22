c20efefc5cf4b3973d86ec554284cb1b3e66a177
RuiLi280 <lr_280@qq.com>
2019-05-21 22:15:07 -0700

    err

--- FinalProject/openhack-client/src/JoinHackathon/JoinHackathon.js
+++ FinalProject/openhack-client/src/JoinHackathon/JoinHackathon.js
@@ -19,7 +19,7 @@ class JoinHackathon extends Component {
             members: [],
             teamLead: "",
             hackathon: {},
-            inviteEmail:""
+            inviteEmail: ""
         };
         this.handleInviteChange = this.handleInviteChange.bind(this);
         this.handleInvite = this.handleInvite.bind(this);
@@ -45,9 +45,9 @@ class JoinHackathon extends Component {
                 }
                 this.setState({hackathon: h, members: memberHolder});
             })
-            .catch( err => {
-                alert(err+"\n"+err.response.data);
-                console.log(err);
+            .catch(err => {
+                alert(err.response.data);
+                // console.log(err);
             });
         this.setState({hid: hid, uid: uid, teamLead: teamLead});
     }
@@ -88,11 +88,11 @@ class JoinHackathon extends Component {
         axios.post(process.env.REACT_APP_API_URL + '/hackathon/team', payload)
             .then(res => {
                 const data = res.data;
-                 console.log("data ",data);
+                console.log("data ", data);
                 this.joinHack(data.id);
             })
             .catch(err => {
-                alert(err+"\n"+err.response.data);
+                alert(err.response.data);
                 console.log(err);
             });
     }
@@ -109,7 +109,7 @@ class JoinHackathon extends Component {
                 // this.props.history.push('/hackathonEvent/' + this.state.hid);
             })
             .catch(err => {
-                alert(err+"\n"+err.response.data);
+                alert(err.response.data);
                 console.log(err);
             });
     }
@@ -141,30 +141,30 @@ class JoinHackathon extends Component {
         return inputs;
     }
 
-    handleInviteChange(e){
-console.log("e ",e);
+    handleInviteChange(e) {
+// console.log("e ",e);
 
         this.setState({inviteEmail: e.target.value});
     }
 
-    
+
     handleInvite(e) {
         e.preventDefault();
-        
+
         alert("Invite sent");
         const payload = {
-            
+
             uid: this.state.uid,
             email: this.state.inviteEmail
         };
-        console.log("e ",e,"payload ",payload);
-        axios.post(process.env.REACT_APP_API_URL+'/invite', payload)
+        console.log("e ", e, "payload ", payload);
+        axios.post(process.env.REACT_APP_API_URL + '/invite', payload)
             .then(res => {
                 alert("invite successful. Please check email to activate");
-                this.setState({inviteEmail:""});
+                this.setState({inviteEmail: ""});
             })
             .catch(err => {
-                alert(err+"\n"+err.response.data);
+                alert(err + "\n" + err.response.data);
                 console.log(err);
             });
     }
@@ -173,23 +173,25 @@ console.log("e ",e);
         return (
             <div>
                 <Header/>
-                
-                
+
+
                 <div>
                     <Form>
                         {/*<h2>Invite external user here</h2>*/}
                         <form class="form-inline" onSubmit={this.handleInvite.bind(this)}>
-                          {<div class="form-group mb-2">
-                            <label for="staticEmail2" class="sr-only">Email</label>
-                            <input type="text" readonly class="form-control-plaintext" id="staticEmail2" value="Invite external user here" />
-                          </div>}
-                          <div class="form-group mx-sm-3 mb-2 ">
-                            
-                            <input type="text" class="form-control" value={this.state.inviteEmail} placeholder="Email Address" onChange={this.handleInviteChange}/>
-                          </div>
-                          <button type="submit" class="btn btn-primary mb-2">Invite</button>
+                            {<div class="form-group mb-2">
+                                <label for="staticEmail2" class="sr-only">Email</label>
+                                <input type="text" readonly class="form-control-plaintext" id="staticEmail2"
+                                       value="Invite external user here"/>
+                            </div>}
+                            <div class="form-group mx-sm-3 mb-2 ">
+
+                                <input type="text" class="form-control" value={this.state.inviteEmail}
+                                       placeholder="Email Address" onChange={this.handleInviteChange}/>
+                            </div>
+                            <button type="submit" class="btn btn-primary mb-2">Invite</button>
                         </form>
-                        
+
                     </Form>
                 </div>
 
